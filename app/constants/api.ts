03cea6cf8b5d049765862e6fc67aa441a8d8e6c1
annaO2AI <annasaheb.kute@o2aicorp.com>
2025-08-22 18:57:07 +0530

    UI fixed

--- app/constants/api.ts
+++ app/constants/api.ts
@@ -1,6 +1,14 @@
+// const API_BASE_URL =
+//   process.env.NEXT_PUBLIC_API_BASE_URL ||
+//   // "https://aisummary-api-usc-geemebfqfmead8f4.centralus-01.azurewebsites.net"
+// // "https://aisummary-api-fue6a9gxabdceng4.canadacentral-01.azurewebsites.net"
+// "https://aisummary-api-fue6a9gxabdceng4.centralus-01.azurewebsites.net"
 
 const API_BASE_URL = "https://ai-call-summary-ap-batch-fjfxdsdhdkd5b7bt.centralus-01.azurewebsites.net"
 
+const API_BASE_URL_AISEARCH =
+  process.env.NEXT_PUBLIC_API_BASE_URL_AISEARCH ||
+  "https://ai-search-recruitment-api-hcg0c3bahzexatab.centralus-01.azurewebsites.net/"
 
 export const API_ROUTES = {
   audioFiles: `${API_BASE_URL}/audio-files`,
@@ -10,4 +18,10 @@ export const API_ROUTES = {
   downloadReport: `${API_BASE_URL}/download-report`,
   useaccess: `${API_BASE_URL}/get-user-role`,
 
+  //AI Search api's
+  upload: `${API_BASE_URL_AISEARCH}/upload`,
+  conversations: `${API_BASE_URL_AISEARCH}/conversations`,
+  ask: `${API_BASE_URL_AISEARCH}/ask`,
+  deleteConversation: (conversation_id: string) =>
+    `${API_BASE_URL_AISEARCH}/conversations/${conversation_id}`,
 }
